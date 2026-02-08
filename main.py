fb6773b9b406d8af16c0e6efbce60a722a5150f6
Devexhhh <kumarsujal.codes@gmail.com>
2026-02-08 22:24:33 +0530

    added main

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+
+from chatbot.rule_based import chatbot_reply
+
+print("🤖 Chatbot is running! Type 'bye' to exit.\n")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() in ["bye", "exit", "quit"]:
+        print("Bot: Goodbye! 👋")
+        break
+
+    response = chatbot_reply(user_input)
+    print(f"Bot: {response}")
